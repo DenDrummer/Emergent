22817ddaba411e4af7c8fc8423581fbbcfa7c477
DenDrummer <muzikant96@gmail.com>
2020-03-28 22:25:29 +0100

    improve movement script
    still not even covering the basics of movement

--- workers/Emergent/Assets/Scripts/PlayerMovement.cs
+++ workers/Emergent/Assets/Scripts/PlayerMovement.cs
@@ -12,11 +12,16 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField]
-    private float mouseSensitivity;
+    private float mouseSensitivity = 100f;
     [SerializeField]
-    private float moveSpeed;
+    private float moveSpeed = 12f;
     [SerializeField]
-    private float jumpHeight;
+    private float jumpHeight = 5f;
+    [SerializeField]
+    private float fallMult = 2.5f;
+    [SerializeField]
+    private float lowJumpMult = 2f;
+
 
     private float mouseX;
     private float mouseY;
@@ -30,24 +35,61 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
         #region --- mouse movement ---
-        mouseX = -Input.GetAxis("Mouse X") * mouseSensitivity;
-        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        mouseX = -Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        mouseY = Mathf.Clamp(mouseY, -90f, 90f);
         //mouseDelta = new Vector3(mouseX, mouseY * -1);
-        //playerCamera.transform.eulerAngles -= new Vector3(mouseY, mouseX);
+        playerCamera.transform.eulerAngles -= new Vector3(mouseY, 0);
         transform.eulerAngles -= new Vector3(0, mouseX);
         #endregion
 
         #region --- movement ---
 
         #region --- cardinal directions ---
-        // TODO: movement in cardinal directions
+        float forward = 0;
+        float right = 0;
+
+        #region --- get input ---
+        // TODO: use buttons instead so it can be remapped by the user.
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            forward += 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            forward -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            right += 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            right -= 1;
+        }
+        #endregion
+
+        #region --- move ---
+        Vector3 move = transform.right * right + transform.forward * forward;
+        move = move * moveSpeed * Time.deltaTime;
+        #endregion
         #endregion
 
         #region --- jump ---
-        if (Input.GetKey(KeyCode.Space))
+        // TODO: check if player is grounded
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, jumpHeight, 0));
+            rb.velocity = Vector3.up * jumpHeight;
+        }
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMult - 1) * Time.deltaTime;
+        } else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMult - 1) * Time.deltaTime;
         }
         #endregion
         #endregion
